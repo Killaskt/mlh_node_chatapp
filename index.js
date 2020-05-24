9690fa4eaa66be605aeec8e23602e98f8985bb89
Shubham Tiwari <thegimreper@gmail.com>
2020-05-23 20:46:49 -0400

    adding more to the server
    habibi

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+const PORT = 3000;
 
-app.get('/api/items', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/index.html');
 });
 
-const PORT = 3000;
+app.get('/thaddg', (req, res) => {
+    res.send('<img src="https://media.discordapp.net/attachments/708421243392622625/710610359144153128/k6jbjcxo1ky41.gif"></img>')
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
 
 http.listen(PORT, () => console.log(`listening on *:${PORT}`));
